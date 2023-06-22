a085a6abd0c916273f32ca29a909dc27c390b0b4
sridhar <135723289+maharaja91@users.noreply.github.com>
2023-06-22 21:15:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -111,7 +111,7 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
 #redict
 MAIN_CHANNEL = environ.get('MAIN_CHANNEL',"https://t.me/Royal_Enfield_bot_Updates")
-FILE_FORWARD = environ.get('FILE_FORWARD',"https://t.me/+xZk8LiD7K2oyODQ1")
+FILE_FORWARD = environ.get('FILE_FORWARD',"https://t.me/+hHHW8TOqFhE1OWQ1")
 MSG_ALRT = environ.get('MSG_ALRT', '𝑪𝑯𝑬𝑪𝑲 & 𝑻𝑹𝒀 𝑨𝑳𝑳 𝑴𝒀 𝑭𝑬𝑨𝑻𝑼𝑹𝑬𝑺')
 FILE_CHANNEL = int(environ.get('FILE_CHANNEL', 0))
 
