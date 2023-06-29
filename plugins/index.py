b3c1ee13ab614ca0139b691632a7c8edc858df8b
sridhar <135723289+maharaja91@users.noreply.github.com>
2023-06-29 22:13:49 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -154,7 +154,7 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
                     can = [[InlineKeyboardButton('Cancel', callback_data='index_cancel')]]
                     reply = InlineKeyboardMarkup(can)
                     await msg.edit_text(
-                        text=f"🔄 Fetched: <code>{current}</code>\n✅ Total saved: <code>{total_files}</code>\n📇 Duplicate Files: <code>{duplicate}</code>\n🗑 Deleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\n⛔️ Invalid: <code>{errors}</code>",
+                        text=f"🔄 Fetched: <code>{current}</code>\n✅ Total saved: <code>{total_files}</code>\n📇 Duplicate Files: <code>{duplicate}</code>\n🗑 Deleted Messages Skipped: <code>{deleted}</code>\n📻Non-Media Files: <code>{no_media + unsupported}</code>\n⛔️ Invalid: <code>{errors}</code>",
                         reply_markup=reply)
                 if message.empty:
                     deleted += 1
@@ -182,4 +182,4 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
             logger.exception(e)
             await msg.edit(f'Error: {e}')
         else:
-            await msg.edit(f'Succesfully saved : <code>{total_files}</code> to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>')
+            await msg.edit(f'✅ Total saved: <code>{total_files}</code>\n📇 Duplicate Files: <code>{duplicate}</code>\n🗑 Deleted Messages Skipped: <code>{deleted}</code>\n📻Non-Media Files: <code>{no_media + unsupported}</code>\n⛔️ Invalid: <code>{errors}</code>')
