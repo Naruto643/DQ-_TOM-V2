a3ae491c5cd8b88cc6dd9170ca690eab703cb486
sridhar <135723289+maharaja91@users.noreply.github.com>
2023-07-01 19:28:18 +0530

    Delete Database_2.py

--- plugins/Database_2.py
+++
@@ -1,9 +0,0 @@
-from pyrogram Client import as Mbot
-from database import dbcount
-from database import dbcount as db
-from database import dbcount as dib
-from pyrogram import  Client  as Mbot
-from sys import platform,
-import psutil
-import requests
-from pyrogram.errors import FloodWait
